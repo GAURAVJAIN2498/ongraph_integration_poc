4fe3229d9d5c7121398dadb0030174a8818a175b
Gaurav Jain <136182348+GAURAVJAIN2498@users.noreply.github.com>
2025-09-02 11:30:54 +0530

    Create index.php

---
+++ task2/index.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="POST" action="login.php">
+        <label>Username:</label>
+        <input type="text" name="username" required><br><br>
+        <label>Password:</label>
+        <input type="password" name="password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
