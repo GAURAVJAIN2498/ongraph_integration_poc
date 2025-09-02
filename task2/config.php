e637222d5d6646bc55414b54cabfafb31527a481
Gaurav Jain <136182348+GAURAVJAIN2498@users.noreply.github.com>
2025-09-02 15:46:10 +0530

    Update config.php

--- task2/config.php
+++ task2/config.php
@@ -1,10 +1,10 @@
 <?php
-$host = "172.17.0.2";
-$user = "testapp";
+$servername = "172.17.0.2";
+$username = "testapp";
 $password = "testpass";
 $dbname = "loginapp";
 
-$conn = new MYSQLi($host, $user, $password, $dbname);
+$conn = new MYSQLi($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
